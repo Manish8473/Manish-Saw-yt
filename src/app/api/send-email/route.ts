93ff59e3a372054739960602fc6ca2c873cc73eb
Manish8473 <manishkr7399@gmail.com>
2026-01-12 08:23:50 +0300

    Update route.ts

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -40,7 +40,7 @@ export async function POST(req: NextRequest) {
   }
 
   const apiKey = process.env.RESEND_API_KEY;
-  if (!apiKey) {
+  if (!apiKey) {re_abtG846Y_AV6urZjX4yE6VzGZjC9ZapCX}
     return NextResponse.json(
       { error: "Missing Resend API Key" },
       { status: 500 }
@@ -52,8 +52,8 @@ export async function POST(req: NextRequest) {
     const resend = new Resend(apiKey);
 
     const { data, error } = await resend.emails.send({
-      from: "From Portfolio <contact@itsniloy.me>",
-      to: ["contact.niloybhowmick@gmail.com"],
+      from: "From Portfolio <manishkr7399@gmail.com>",
+      to: ["manishkr7399@gmail.com"],
       subject: `New Message from Portfolio - ${projectType || "General Inquiry"}`,
       react: EmailTemplate({ name, email, message, projectType, timeline }),
     });
